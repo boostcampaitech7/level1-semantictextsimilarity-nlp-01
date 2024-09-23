fc4f2101167f1a90959d359755fb1f1521bd408e
swbaek97 <swbaek97@gmail.com>
2024-09-23 13:29:27 +0000

    Feat: Label_5 data augmentation & label_0 data under sampling

--- src/data_pipeline/augmentation.py
+++ src/data_pipeline/augmentation.py
@@ -26,6 +26,7 @@ class Augmentation():
     
     def __call__(self, data):
         augmented_data = []
+        sampled_data = []
         for idx, item in tqdm(data.iterrows(), desc='augmenting', total=len(data)):
             for aug in self.augmentation:
                 map_func = self.mappings[aug["method"]](**aug["params"])
@@ -33,7 +34,18 @@ class Augmentation():
                     augmented = map_func(item)
                     if augmented.empty:
                         continue
-                    augmented_data.append(augmented)
+                    if aug["method"] == 'undersample_label_0':
+                        sampled_data.append(augmented)
+                    else:
+                        augmented_data.append(augmented)
         augmented_data.append(data)
-        rtn = pd.concat(augmented_data, ignore_index=True)
+        augmented_data_df = pd.concat(augmented_data)
+        sampled_data_df = pd.concat(sampled_data)
+        tmp = pd.concat([augmented_data_df, sampled_data_df]).drop_duplicates(keep=False)
+        sampled_data_df['sentence_2'] = sampled_data_df['sentence_1']
+        sampled_data_df['label'] = 5.0
+        sampled_data_df['binary-label'] = 1.0
+
+        rtn = pd.concat([tmp, sampled_data_df]).drop_duplicates(keep=False)
+        # rtn = pd.concat(augmented_data)
         return rtn
--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -4,10 +4,3 @@ def augment_func(name):
         func.call_name = name
         return func
     return decorator
-
-def under_sampling_func(name):
-    def decorator(func):
-        func.is_under_sampling_func = True
-        func.call_name = name
-        return func
-    return decorator
